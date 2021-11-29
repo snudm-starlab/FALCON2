6e33facee8df1804884df79378374d6e669863bd
ant6si <50072144+ant6si@users.noreply.github.com>
2021-11-29 15:32:48 +0900

    Update imagenet.py

--- src/train_test/imagenet.py
+++ src/train_test/imagenet.py
@@ -611,7 +611,6 @@ def save_checkpoint(state, is_best, checkpoint='checkpoint', filename='checkpoin
 #def adjust_learning_rate(optimizer, epoch, args):
 #    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
 #    lr = args.lr * (0.1 ** (epoch // 30))
-#    for param_group in optimizer.param_groups:
 
 def adjust_learning_rate(optimizer, epoch, iteration, num_iter, args):
     '''
